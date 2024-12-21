a398e207e579766fdcde278ba0002192935007c0
bvakilfy25 <bvakil@deloitte.com>
2024-12-20 19:52:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,4 @@ if ingredients_list:
 # New section to display smoothiefront nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
