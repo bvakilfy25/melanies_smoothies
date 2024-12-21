a6e26911dfcf60fae6923d54a82cc7b8fcc45975
bvakilfy25 <bvakil@deloitte.com>
2024-12-20 20:29:02 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,11 +14,15 @@ st.write("The name on your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
 # Convert Smowpark Dataframe to Pandas Dataframe
 pd_df = my_dataframe.to_pandads()
-# st.dataframe(data=my_dataframe, use_container_width=True)
-# st.stop
+st.dataframe(pd_df)
+st.stop()
+
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:", my_dataframe, max_selections = 5
